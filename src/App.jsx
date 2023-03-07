5072d9592501b0e921448f8955218cb6d8711f90
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-07 19:10:58 +0600

    Practice start router

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 const router = createBrowserRouter()
-
 function App() {
   return (
     <div className="App">
