f539457290e16f7414fe04056bee3f058f8c7421
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-10 00:26:42 +0600

    Practice start router

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is header</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is header</h1>
+    </div>
+  );
 };
 
-export default Header;<h1>This is header</h1>
+export default Header;
