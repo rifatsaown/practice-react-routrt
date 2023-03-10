5b467847075f7049a7ad36e28db2e4e9f3696cc5
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-11 00:27:09 +0600

    Practice start router

--- src/Friend/Friend.jsx
+++ src/Friend/Friend.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Friend = () => {
-    return (
-        <div>
-            <h1>This is Friend</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is Friend</h1>
+    </div>
+  );
 };
 
 export default Friend;
