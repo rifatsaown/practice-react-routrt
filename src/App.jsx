7897cea2e4583c35f3605ce223b2215414080d37
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-13 15:29:52 +0600

    Nested Routing Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home/Home';
+import './App.css'
+import Friend from './Friend/Friend';
+import About from './About/About';
 
-const router = createBrowserRouter();
-function App() {
+const router = createBrowserRouter([
+  { path: '/',element: <Home /> },
+  { path: '/home',element: <Home /> },
+  { path: '/about',element: <About/>},
+  { path: '/friend',element: <Friend /> },
+])
+
+const App = () => {
   return (
-    <div className="App">
-      <RouterProvider router={router} />
+    <div className='App'>
+      <RouterProvider router={router}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <h1>This is header</h1>
+      <Link to='/home'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/friend'>Friend</Link>
     </div>
   );
 };
