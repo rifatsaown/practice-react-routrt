fbcfeed308ace6368ac8c460a52f50b0c2dfcbaa
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-13 15:52:38 +0600

    design done nested routing done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Home/Home';
 import './App.css'
-import Friend from './Friend/Friend';
+import Friends from './Friends/Friends';
 import About from './About/About';
+import Main from './Main/Main';
 
 const router = createBrowserRouter([
-  { path: '/',element: <Home /> },
-  { path: '/home',element: <Home /> },
-  { path: '/about',element: <About/>},
-  { path: '/friend',element: <Friend /> },
+  { path: '/',element: <Main/> 
+  ,children: [
+    { path: '/',element: <Home/>},
+    { path: '/home',element: <Home/>},
+    { path: '/about',element: <About/>},
+    { path: '/friend',element: <Friends/>},
+  ]},
 ])
 
 const App = () => {
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header">
+      <h2 style={{color : "purple"}}>RS</h2>
+      <nav>
       <Link to='/home'>Home</Link>
       <Link to='/about'>About</Link>
       <Link to='/friend'>Friend</Link>
+      </nav>
     </div>
   );
 };
